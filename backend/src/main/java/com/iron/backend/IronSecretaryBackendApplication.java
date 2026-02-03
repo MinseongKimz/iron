223cc697f9621638176767ebfbfdfcc613156e04
MinseongKimz <minseongkimC@gmail.com>
2026-02-03 01:51:06 +0000

    Initial commit: Initialize Spring Boot (Gradle) Backend and Next.js Frontend with Doc

---
+++ backend/src/main/java/com/iron/backend/IronSecretaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iron.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronSecretaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronSecretaryBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/iron/backend/IronSecretaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iron.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronSecretaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
